8782e94efd827426fad35f0578a9e97406601489
Chetan24k <chetankharote2@gmail.com>
2022-12-22 17:09:59 +0530

    implementing guessing number functionality

--- 3.mini project/script.js
+++ 3.mini project/script.js
@@ -1,1 +1,21 @@
 'use strict';
+//genrate random number in range
+
+
+
+let randomNumber =Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+console.log(randomNumber);
+
+document.querySelector('.number').textContent = randomNumber;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess=Number( document.querySelector('.guess').value);
+    if(!guess)
+    {
+        document.querySelector('.message').textContent ="No Number😐";
+    }
+    else if(guess===randomNumber)
+    {
+        document.querySelector('.message').textContent ="Correct Number😐";
+    }
+})
