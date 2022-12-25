d0ffcc09a265d1c0996d29305dab0625543f6bc7
Chetan24k <chetankharote2@gmail.com>
2022-12-25 12:15:23 +0530

    implementing show js ad window and close ad window

--- 4.modal project/starter/script.js
+++ 4.modal project/starter/script.js
@@ -1,2 +1,26 @@
 'use strict';
+const modal=document.querySelector(".modal");
+const overlay=document.querySelector(".overlay");
+const btnCloseModal=document.querySelector(".close-modal");
+const btnShowModal=document.querySelectorAll(".show-modal");
+console.log(btnShowModal[1]);
 
+let showModal=function()
+{
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+let closeModal=function()
+{
+      overlay.classList.add("hidden");
+      console.log("hi");
+      modal.classList.add("hidden");  
+}
+
+for(let i=0;i<btnShowModal.length;i++)
+{
+    btnShowModal[i].addEventListener("click",showModal);
+}
+btnCloseModal.addEventListener("click",closeModal);
+overlay.addEventListener("click",closeModal);
