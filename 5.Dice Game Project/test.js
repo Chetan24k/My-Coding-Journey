66588faf3397ea629c16b932745fa9e8aadd595a
Chetan24k <chetankharote2@gmail.com>
2022-12-27 21:08:14 +0530

    final commit

--- 5.Dice Game Project/test.js
+++ 5.Dice Game Project/test.js
@@ -15,14 +15,27 @@ const btnHold=document.querySelector(".btn--hold");
  
 
 //starting condtion
-score0El.textContent=0;
-score1El.textContent=0;
-diceEl.classList.add("hidden");
 
-const scores=[0,0];
-let currentScore=0;
-let activePlayer=0;
-let playing=true;
+
+const scores =[0,0];
+let currentScore;
+let activePlayer;
+let playing;
+
+const init=function()
+{
+    currentScore=0;
+    activePlayer=0;
+    playing=true;
+    score0El.textContent=0;
+    score1El.textContent=0;
+    diceEl.classList.add("hidden");
+    document.querySelector(`.player--${activePlayer}`).classList.remove(`player--winner`);
+    player1El.classList.remove('player--active');
+    player0El.classList.add('player--active');
+}
+
+init();
 
 let swtichPlayer=function()
 {
@@ -75,6 +88,8 @@ btnHold.addEventListener("click",function()
    if(scores[activePlayer]>=20)
    {
         playing=false;
+        diceEl.classList.add("hidden");
+
         document.querySelector(`.player--${activePlayer}`).classList.add(`player--winner`);
         document.querySelector(`.player--${activePlayer}`).classList.remove(`player--active`);
 
@@ -88,4 +103,6 @@ btnHold.addEventListener("click",function()
   
 
 })
+
+btnNew.addEventListener("click",init);
 
