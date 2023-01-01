13b3cb5aa2e48d79fa73e70aa657e5188e31101f
Chetan24k <chetankharote2@gmail.com>
2023-01-01 20:36:36 +0530

    hoisting testing

---
+++ 6.How JS works/test2.js
@@ -0,0 +1,36 @@
+// const myname="chetan";
+// if(myname==="chetan")
+// {
+//     console.log(`chetan is a${job}`);
+//     const age=22;
+//     console.log(age);
+//     let job="eng";
+//     console.log(x);
+// }
+
+// console.log(a);
+console.log(b);
+let a=10;
+console.log(a);
+var b=20;
+console.log(b);
+
+console.log(funDecl(2,3));
+// console.log(funExp(2,3));
+// console.log(funArr(2,3));
+
+
+function funDecl(a,b)
+{
+    return a+b;
+}
+
+let funExp=function(a,b)
+{
+    return a+b;
+}
+
+let funArr=(a,b)=>
+{
+    return a+b;
+}
