c6fbfe5ec9115cd423a03f4f33bb0f16dd27ce85
Chetan24k <chetankharote2@gmail.com>
2023-02-06 17:30:57 +0530

    for each method use

---
+++ 9-Arrays-Bankist/lecture practice/forEach.js
@@ -0,0 +1,11 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+
+movements.forEach(function(movement,i,arr)
+{
+    console.log(i,arr,movement);
+});
+
+//it calls the callback function on each element of the array and first parameter 
+//represents the value at each index in array
+
