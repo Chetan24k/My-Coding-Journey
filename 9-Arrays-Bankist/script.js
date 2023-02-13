fe7f52d9a85fa468e459d3de93feda70511497d8
Chetan24k <chetankharote2@gmail.com>
2023-02-13 16:03:05 +0530

    implemented loan functionlity

--- 9-Arrays-Bankist/script.js
+++ 9-Arrays-Bankist/script.js
@@ -229,3 +229,19 @@ btnClose.addEventListener("click",function(e)
 
 
 })
+
+//some method
+btnLoan.addEventListener("click",function(e)
+{
+  e.preventDefault();
+  const amount=Number(inputLoanAmount.value);
+  if(amount>0&&currentAccount.movements.some(mov=>mov>=amount*0.1))
+  {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+})
+
+//every method
+
+ 
