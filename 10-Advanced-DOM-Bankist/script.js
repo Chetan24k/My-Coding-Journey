a1c426c8a02728170057e5bfd44431d5c9559b75
Chetan24k <chetankharote2@gmail.com>
2023-02-14 17:45:26 +0530

    learnt how to create ,select & delete html element

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+ 
 ///////////////////////////////////////
 // Modal window
 
@@ -8,18 +8,29 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function(e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
+
+//we have two butoon so to add eventlistener on both button we 
+//use for loop
+// console.log(btnsOpenModal);//prints 2 nodes with elements that have
+//.btn--show-modal in their class,
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+
+btnsOpenModal.forEach(Element=>Element.addEventListener("click",openModal));
+
+
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +40,35 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+//selecting html element
+console.log(document.documentElement);
+console.log(document.body);
+
+const header= document.querySelector(".header");
+
+
+//creating and inserting elements
+const msg=document.createElement("div");
+//this only creates the html element but it is not inserted into page yet
+
+msg.classList.add("cookie-message");
+// msg.textContent="we use cookies for improved "
+msg.innerHTML=`we use cookies for improved functionality <button class="btn btn--close-cookie">Got it!</button> `;
+header.prepend(msg);
+//adds element as the first child of element
+header.append(msg);
+//adds element as the last child
+// here only one element is present at the end and as element is first  prepended 
+//it is not duplicated  and only moved
+
+header.before(msg);
+//inserts msg before header as a sibling
+header.after(msg);
+
+//delete element
+document.querySelector('.btn--close-cookie').addEventListener('click',function()
+{
+  msg.remove();
+  //earlier method
+  // msg.parentElement.removeChild(msg);
+})
