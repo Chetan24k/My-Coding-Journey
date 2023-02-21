1507b9f87a0fa46529433c50cbace3eaa85a5e12
Chetan24k <chetankharote2@gmail.com>
2023-02-21 16:00:08 +0530

    prototypes

--- 11-OOP/script.js
+++ 11-OOP/script.js
@@ -10,13 +10,26 @@ const person=function(firstName, birthYear)//construtor function
 }
 //cant use arrow function as it does not have this keyword 
 let chetanObj=new person("chetan",1999);
-// new empty on=bject is created 
-// functin is called and this points to empty object 
-// objects is linked to prototype
-// function automatically returns new object
+//1. new empty on=bject is created 
+//2.  functin is called and this points to empty object 
+//3.  objects is linked to prototype
+//4.  function automatically returns new object
 console.log(chetanObj);
 let manasviObj=new person("manasvi",2000);
 console.log(manasviObj);
 let vijay="vijay";
 console.log(chetanObj instanceof  person);
 console.log(vijay instanceof person);
+person.prototype.pin=421503;
+chetanObj.__proto__.age=21;
+console.log(manasviObj);
+console.log(person.prototype);
+
+
+let obj={
+    name:"hi"
+}
+obj.__proto__.points=22;
+console.log(obj.__proto__);
+
+
