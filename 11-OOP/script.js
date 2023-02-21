17485c1966f62ec2ddc3df15092235ef3b78fa37
Chetan24k <chetankharote2@gmail.com>
2023-02-21 11:55:08 +0530

    constructor function and new object in js

---
+++ 11-OOP/script.js
@@ -0,0 +1,22 @@
+'use strict';
+const person=function(firstName, birthYear)//construtor function 
+{
+    //object variable are called properties
+    this.fname=firstName;
+    this.by=birthYear;
+    //we dont create methods in constructor functions as for thousands of objects thousands of methods will be created and it will cause 
+    // performance issues
+    // console.log(this);//person {} object of type person
+}
+//cant use arrow function as it does not have this keyword 
+let chetanObj=new person("chetan",1999);
+// new empty on=bject is created 
+// functin is called and this points to empty object 
+// objects is linked to prototype
+// function automatically returns new object
+console.log(chetanObj);
+let manasviObj=new person("manasvi",2000);
+console.log(manasviObj);
+let vijay="vijay";
+console.log(chetanObj instanceof  person);
+console.log(vijay instanceof person);
