00cc5d614eab3f3a6b689a325991cd47e738e2b8
Chetan24k <chetankharote2@gmail.com>
2023-03-02 00:15:47 +0530

    rendering form on click

--- 12-Mapty Project/script.js
+++ 12-Mapty Project/script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+console.log(map);
 if(navigator.geolocation)
 {
 navigator.geolocation.getCurrentPosition(function(position)
@@ -18,13 +18,20 @@ navigator.geolocation.getCurrentPosition(function(position)
   let {latitude,longitude}=position.coords
   let url = `https://www.google.com/maps/@${latitude},${longitude}`;
 
-    let cordinates=[latitude,longitude];
-  var map = L.map('map').setView(cordinates, 14);
+  let cordinates=[latitude,longitude];
+  let map = L.map('map').setView(cordinates, 14);
 
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+map.on("click",function(mapE)
+{
+
+  form.classList.remove("hidden");
+  
+})
 
 L.marker(cordinates).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
@@ -36,3 +43,24 @@ function()
     alert("could not get your co-ords")
 })
 };
+form.addEventListener("submit",function(e)
+{
+  e.preventDefault();
+
+  let {lat,lng}=mapE.latlng;
+  L.marker([lat,lng]).addTo(map)
+    .bindPopup(L.popup({
+      maxWidth:100,
+      minWidth:100,
+      autoClose:false,
+      closeOnClick:false,
+      className:"running-popup"
+    }))
+    .setPopupContent("Walking")
+    .openPopup();
+})
+inputType.addEventListener("change",function(e)
+{
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+})
